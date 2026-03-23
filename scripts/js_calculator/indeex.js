6b7ae35e8348ef64cb440f89d160ca231650234f
tesfay <tesfayhailemariam29@gmail.com>
2026-03-23 09:47:52 +0300

    function 3

--- scripts/js_calculator/indeex.js
+++ scripts/js_calculator/indeex.js
@@ -5,3 +5,11 @@ function appendToDisplay(input){
 function clearDisplay(){
     display.value = '';
 }
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    } 
+    catch (error) {
+        display.value = 'Error';
+    }
+}
